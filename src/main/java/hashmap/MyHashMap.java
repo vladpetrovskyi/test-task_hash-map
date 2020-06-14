efecfa60a105ddfc155401d0a352926b870eab44
Vlad Petrovskyi <62035381+vladpetrovskyi@users.noreply.github.com>
2020-06-14 18:48:33 +0300

    Solution (#1)
    * added travis ci
    
    * added JUnit dependency
    
    * added some basic put and get tests
    
    * implemented put, get and size methods
    
    * added SonarCloud support
    
    * added parametrized tests support
    
    * implemented tests
    
    * implemented own HashMap with methods get, put and size

---
+++ src/main/java/hashmap/MyHashMap.java
@@ -0,0 +1,98 @@
+package hashmap;
+
+public class MyHashMap {
+
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.7f;
+    private static final long DEFAULT_VALUE = -1;
+    private final float loadFactor;
+    private int arrayCapacity;
+    private int elementsQuantity = 0;
+    private Node[] nodesArray;
+
+    public MyHashMap() {
+        loadFactor = DEFAULT_LOAD_FACTOR;
+        nodesArray = new Node[DEFAULT_CAPACITY];
+        arrayCapacity = DEFAULT_CAPACITY;
+    }
+
+    public MyHashMap(int capacity) {
+        loadFactor = DEFAULT_LOAD_FACTOR;
+        nodesArray = new Node[capacity];
+        arrayCapacity = capacity;
+    }
+
+    public MyHashMap(int capacity, float loadFactor) {
+        this.loadFactor = loadFactor;
+        nodesArray = new Node[capacity];
+        arrayCapacity = capacity;
+    }
+
+    public void put(int key, long value) {
+        if (elementsQuantity >= arrayCapacity * loadFactor) {
+            resize();
+        }
+
+        int index = getIndex(key);
+        while (nodesArray[index] != null && nodesArray[index].key != key) {
+            index++;
+            if (index >= arrayCapacity) {
+                resize();
+                index = getIndex(key);
+            }
+        }
+
+        if (nodesArray[index] != null && nodesArray[index].key == key) {
+            nodesArray[index].value = value;
+        } else {
+            nodesArray[index] = new Node(key, value);
+            elementsQuantity++;
+        }
+    }
+
+    public long get(int key) {
+        int index = getIndex(key);
+        while (index < arrayCapacity && nodesArray[index] != null && nodesArray[index].key != key) {
+            index++;
+        }
+
+        if (index >= arrayCapacity || nodesArray[index] == null) {
+            return DEFAULT_VALUE;
+        }
+        return nodesArray[index].value;
+    }
+
+    public int size() {
+        return elementsQuantity;
+    }
+
+    private static class Node {
+        int key;
+        long value;
+
+        Node(int key, long value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private void resize() {
+        arrayCapacity *= 2;
+        Node[] newTable = new Node[arrayCapacity];
+        for (Node node : nodesArray) {
+            if (node == null) {
+                continue;
+            }
+            int index = getIndex(node.key);
+            while (newTable[index] != null) {
+                index++;
+            }
+            newTable[index] = node;
+        }
+        nodesArray = newTable;
+    }
+
+    private int getIndex(int key) {
+        return Math.abs(key) % arrayCapacity;
+    }
+}
